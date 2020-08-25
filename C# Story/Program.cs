a2b0b29f03187e03ce7e10dd2b916e0c736578f8
Muhammad Irtaza <66386453+Irtaza2009@users.noreply.github.com>
2020-08-25 09:38:24 -0700

    Add files via upload

---
+++ C# Story/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace Story
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Once upon a time there was a boy named ......");
+            Console.ReadLine();
+            Console.WriteLine("He was very .....");
+            Console.ReadLine();
+            Console.WriteLine("One day he was collecting sticks from the ....");
+            Console.ReadLine();
+            Console.WriteLine("He saw a lion coming ....");
+            Console.ReadLine();
+            Console.WriteLine("The lion was really ....");
+            Console.ReadLine();
+            Console.WriteLine("The boy used his comman sense and climbed a tall ....");
+            Console.ReadLine();
+            Console.WriteLine("and he was saved.");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine("The End!");
+
+            Console.ReadKey();
+        }
+    }
+}
